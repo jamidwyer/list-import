4fa5e8090179351341418a32d7dd2238be3cccc2
Jami Dwyer <jami.dwyer@walmartlabs.com>
2019-09-19 18:28:32 -0700

    updates, start adding lists functionality

---
+++ services/lists/src/db/seeds/lists.js
@@ -0,0 +1,10 @@
+exports.seed = (knex) => {
+  return knex('lists').del()
+  .then(() => {
+    knex('lists').insert({
+      user_id: 1,
+      title: 'groceries',
+    })  // eslint-disable-line
+  })
+  .catch((err) => { console.log(err); }); // eslint-disable-line
+};
